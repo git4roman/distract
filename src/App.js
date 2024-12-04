ebb4bb06d408ae45efe4eff778ee2f913170a308
git4roman <romanforgit@gmail.com>
2024-12-04 19:38:16 +0545

    App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Namaste, Roman Khatri</h1>
       </header>
     </div>
+  );
+}
+
+function About() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>About Page</h1>
+      </header>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
